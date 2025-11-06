a3a35a033de2e6b7693bdee05512c8f9ae9e61b9
Cristian Lenin Roblero <cristianroblero00@gmail.com>
2025-11-06 18:26:20 -0500

    Corrijo script para instalar dependencias antes del despliegue

--- render_build.sh
+++ render_build.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
-# Script de despliegue para Render
+# Script de despliegue para Render (Django)
+
+set -o errexit  # Detiene el script si ocurre un error
+
+echo "📦 Instalando dependencias..."
+pip install --upgrade pip
+pip install -r requirements.txt
+
+echo "🧱 Ejecutando migraciones..."
 python manage.py collectstatic --noinput
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
+
+echo "✅ Despliegue completado correctamente"
