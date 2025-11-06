407624d819da56d12ddd2b17d337f86728bb0648
Cristian Lenin Roblero <cristianroblero00@gmail.com>
2025-11-06 17:51:26 -0500

    Configuración PostgreSQL lista para Render

--- adoption_pet/settings.py
+++ adoption_pet/settings.py
@@ -86,14 +86,28 @@ WSGI_APPLICATION = 'adoption_pet.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.postgresql',
+   #     'NAME': 'adoption_pet_bd', 
+    #    'USER': 'postgres',      
+     #   'PASSWORD': 'R0bl3r0.',      
+      #  'HOST': 'localhost',            
+       # 'PORT': '5432',   
+    #}
+#}
+
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'adoption_pet_bd', 
-        'USER': 'postgres',      
-        'PASSWORD': 'R0bl3r0.',      
-        'HOST': 'localhost',            
-        'PORT': '5432',   
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
